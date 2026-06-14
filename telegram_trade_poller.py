f498252f470e9cbbf897a30c5e76ba7ca4a28552
GitHub Action <action@github.com>
2026-06-14 23:21:54 +0530

    security: remove hardcoded Telegram token, require env vars

--- telegram_trade_poller.py
+++ telegram_trade_poller.py
@@ -19,8 +19,11 @@ import time
 import requests
 from datetime import datetime
 
-BOT_TOKEN   = os.environ.get('TELEGRAM_BOT_TOKEN', '8253327701:AAGNFzBJ8QwKw8x8Hg-tlvWHg18DD4lgogQ')
-CHAT_ID     = os.environ.get('TELEGRAM_CHAT_ID', '8901309420')
+BOT_TOKEN   = os.environ.get('TELEGRAM_BOT_TOKEN', '')
+CHAT_ID     = os.environ.get('TELEGRAM_CHAT_ID', '')
+
+if not BOT_TOKEN or not CHAT_ID:
+    raise RuntimeError("TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID must be set as environment variables. Never hardcode tokens.")
 API         = f"https://api.telegram.org/bot{BOT_TOKEN}"
 PLACE_ORDER = 'http://127.0.0.1:5000/api/place-order'
 RADAR_FILE  = '/home/ubuntu/radar_trades.json'
